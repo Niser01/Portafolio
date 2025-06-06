fcac0737b9cef6d5b9be2eab65630e68a2c79d05
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-06-06 15:12:10 -0500

    Actualización Hero
    Se actualiza el hero de la pagina web

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <nav class="fixed top-4 left-1/2 transform -translate-x-1/2 bg-[#0F172A] shadow-lg rounded-[30px] px-20 py-5 z-50">
-  <div class="flex space-x-8">
+  <div class="flex space-x-25">
     <a href="#hero" class="text-white hover:text-blue-400">Inicio</a>
     <a href="#proyectos" class="text-white hover:text-blue-400">Proyectos</a>
     <a href="#servicios" class="text-white hover:text-blue-400">Servicios</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
-<section id="hero" class="h-screen flex items-center" style="background-color: #1E293B;">
-  <div class="w-full flex flex-col md:flex-row h-full">
+<section id="hero" class="relative h-screen flex items-center justify-center text-white overflow-hidden">
+  <div class="absolute inset-0 z-0" style="
+    background: linear-gradient(to right, #062C70 50%, black 150%);
+  "></div>
 
+  <div class="w-full flex flex-col md:flex-row h-full relative z-10">
     <div class="text-white md:w-1/2 flex flex-col justify-center h-full space-y-4">
       <div class="max-w-7xl mx-auto px-4">
         <p class="text-[32px] font-normal">¡Hola! Soy Nicolás,</p>
@@ -8,12 +11,14 @@
       </div>
     </div>
 
-    <div class="md:w-1/2 h-full">
-        <img src="/home_image.png" alt="Imagen de fondo" class="w-full h-full object-cover"/>
+    <div class="md:w-1/2 h-full flex items-center justify-center relative">
+      <div class="w-120 h-120 bg-white rounded-full shadow-lg absolute z-0"></div>
+      <img src="/imagen_banner.png" alt="Mockup computador" class="w-200 h-200 object-contain relative z-10 opacity-0 translate-y-4 animate-fadeInUp"/>
+
     </div>
+
   </div>
 </section>
-
 
 <section id="proyectos" class="h-screen flex items-center justify-center bg-white">
   <h2 class="text-4xl">Proyectos</h2>
