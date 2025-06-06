b7ffc6ff0b5fdb7c284e5e9ee59dcc6390fe7ddd
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-06-06 15:28:25 -0500

    Actualizacion Hero
    Se hace la actualizacion del Hero para cumplir con el diseño de Figma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <section id="hero" class="relative h-screen flex items-center justify-center text-white overflow-hidden">
-  <div class="absolute inset-0 z-0" style="
-    background: linear-gradient(to right, #062C70 50%, black 150%);
-  "></div>
+  <div class="absolute inset-0 z-0" style="background: linear-gradient(to right, #062C70 50%, black 150%);"></div>
 
   <div class="w-full flex flex-col md:flex-row h-full relative z-10">
     <div class="text-white md:w-1/2 flex flex-col justify-center h-full space-y-4">
       <div class="max-w-7xl mx-auto px-4">
         <p class="text-[32px] font-normal">¡Hola! Soy Nicolás,</p>
-        <h1 class="text-[48px] font-extrabold leading-tight">Desarrollador de Software</h1>
+        <h1 class="text-[48px] font-extrabold leading-tight bg-gradient-to-r from-[#33AAFF] to-[#D711FF] bg-clip-text text-transparent">Desarrollador de Software</h1>
       </div>
     </div>
 
